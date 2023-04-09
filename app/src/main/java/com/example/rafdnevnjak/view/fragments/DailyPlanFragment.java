f14ac9394125f485ea2a720a04d62ac144b68a42
jovanoski1 <mihailojovanoski14@gmail.com>
2023-04-09 21:49:24 +0200

    dailyPlan fragment styling

--- app/src/main/java/com/example/rafdnevnjak/view/fragments/DailyPlanFragment.java
+++ app/src/main/java/com/example/rafdnevnjak/view/fragments/DailyPlanFragment.java
@@ -1,5 +1,6 @@
 package com.example.rafdnevnjak.view.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -15,8 +16,8 @@ import com.example.rafdnevnjak.viewmodels.MyDateSelectedViewModel;
 
 public class DailyPlanFragment extends Fragment {
 
-    private TextView textView;
     private MyDateSelectedViewModel myDateSelectedViewModel;
+    private TextView dateTV;
 
     public DailyPlanFragment(){super(R.layout.fragment_dailyplan);}
 
@@ -29,16 +30,18 @@ public class DailyPlanFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         myDateSelectedViewModel = new ViewModelProvider(requireActivity()).get(MyDateSelectedViewModel.class);
-        textView = view.findViewById(R.id.prokic);
+        dateTV = view.findViewById(R.id.selected_date);
         System.out.println("USAO DAILYPLAN FRAGMENT");
 
         initObservers(view);
     }
 
+    @SuppressLint("SetTextI18n")
     private void initObservers(View view){
         myDateSelectedViewModel.getDate().observe(getViewLifecycleOwner(), e->{
             System.out.println("Selektovan "+e.getDate());
-            textView.setText(e.getDate()+"");
+            String month = e.getDate().getMonth().toString();
+            dateTV.setText(month.substring(0,1).toUpperCase()+month.substring(1).toLowerCase() + " "+ e.getDate().getDayOfMonth()+". "+e.getDate().getYear()+".");
         });
     }
 }
